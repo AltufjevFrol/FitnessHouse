56e95b4a1202f3e301d1f1d37b6b46e5a3bd2a55
Pilipenko Artem <artem.mihailovich@gmail.com>
2019-11-18 02:13:09 +0300

    Add files via upload

---
+++ precache-manifest.29e733caa2effbf2d76d687017774952.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09da8225960635f4f4826a96843a1044",
+    "url": "/FitnessHouse/index.html"
+  },
+  {
+    "revision": "471409ebb1dd222aa227",
+    "url": "/FitnessHouse/static/css/main.75109201.chunk.css"
+  },
+  {
+    "revision": "c3dbcf0cba866bc45d62",
+    "url": "/FitnessHouse/static/js/2.eeff26d3.chunk.js"
+  },
+  {
+    "revision": "471409ebb1dd222aa227",
+    "url": "/FitnessHouse/static/js/main.5ee7d589.chunk.js"
+  },
+  {
+    "revision": "52c934b0a5f1e413e610",
+    "url": "/FitnessHouse/static/js/runtime-main.29def547.js"
+  }
+]);
